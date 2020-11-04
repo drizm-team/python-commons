22a1df592e4be36a72b84d87b39cc9563106f05e
ThaRising <kochbe.ber@gmail.com>
2020-11-04 17:45:33 +0100

    Full introspection API refactor

--- drizm_commons/sqla/inspect.py
+++ drizm_commons/sqla/inspect.py
@@ -1,43 +1,52 @@
 from abc import ABC, abstractmethod
+from inspect import isclass
 from typing import Union
+
+from sqlalchemy import Table
 from sqlalchemy.inspection import inspect
+from sqlalchemy.orm.exc import UnmappedClassError
+from sqlalchemy.orm.util import class_mapper
+
+
+def is_mapped_class(cls) -> bool:
+    if not isclass(cls):
+        return False
+    try:
+        class_mapper(cls)
+    except UnmappedClassError:
+        return False
+    else:
+        return True
 
 
 class _IntrospectorInterface(ABC):
+    tablename: str
+    columns: dict
+    __table__: Table
+
     def __init__(self, obj) -> None:
         self.schema = obj
-        self.tablename = obj.__tablename__
-        self.classname = obj.__name__
 
+    @property
     @abstractmethod
-    def primary_keys(self) -> list:
+    def classname(self):
         pass
 
-    @abstractmethod
-    def unique_keys(self) -> list:
-        pass
-
-    @abstractmethod
-    def foreign_keys(self, columns_only: bool = False) -> Union[list, dict]:
-        pass
-
-
-class _declBaseIntrospector(_IntrospectorInterface):
     def primary_keys(self, retrieve_constraint: bool = False) -> list:
-        constraints = inspect(self.schema).primary_key
+        constraints = inspect(self.__table__).primary_key
         if retrieve_constraint:
             return constraints
         return [c.key for c in constraints.columns]
 
     def unique_keys(self) -> list:
         return [
-            c.name for c in self.schema.c if any(
+            c.name for c in self.columns if any(
                 [c.primary_key, c.unique]
             )
         ]
 
     def foreign_keys(self, columns_only: bool = False) -> Union[list, dict]:
-        foreign_keys = [c for c in self.schema.c if c.foreign_keys]
+        foreign_keys = [c for c in self.columns if c.foreign_keys]
         fk_names = [key.name for key in foreign_keys]
         if columns_only:
             return fk_names
@@ -49,9 +58,64 @@ class _declBaseIntrospector(_IntrospectorInterface):
         }
 
 
+class _declBaseIntrospector(_IntrospectorInterface):
+    """
+    Inspect Declarative Base class instances.
+    These are objects you may get back when querying,
+    using a Session or when constructing an instance yourself
+    """
+    def __init__(self, obj) -> None:
+        super().__init__(obj)
+        self.tablename = obj.__tablename__
+        self.columns = obj.__table__.c
+        self.__table__ = obj.__table__
+
+    @property
+    def classname(self) -> str:
+        return self.schema.__class__.__name__
+
+
+class _declClassIntrospector(_IntrospectorInterface):
+    def __init__(self, obj) -> None:
+        super().__init__(obj)
+        self.tablename = obj.__tablename__
+        self.columns = obj.__table__.c
+        self.__table__ = obj.__table__
+
+    @property
+    def classname(self) -> str:
+        return self.schema.__name__
+
+
+class _tblIntrospector(_IntrospectorInterface):
+    def __init__(self, obj: Table) -> None:
+        super().__init__(obj)
+        self.tablename = obj.name
+        self.columns = obj.c
+        self.__table__ = obj
+
+    @property
+    def classname(self):
+        raise NotImplementedError(
+            f"Class {self.__class__.__name__} does not support "
+            f"declarative base features"
+        )
+
+
 def SQLAIntrospector(o):
-    """ Factory for returning a matching introspector class """
-    return _declBaseIntrospector(o)
+    """ Factory returning a matching introspector class """
+    err_msg = "Class of type {0} is not supported for introspection"
+    if isinstance(o, Table):
+        return _tblIntrospector(o)
+
+    if isclass(o):
+        if is_mapped_class(o):
+            return _declClassIntrospector(o)
+        raise TypeError(err_msg.format(type(o)))
+    else:
+        if is_mapped_class(o.__class__):
+            return _declBaseIntrospector(o)
+        raise TypeError(err_msg.format(type(o.__class__)))
 
 
-__all__ = ["SQLAIntrospector"]
+__all__ = ["SQLAIntrospector", "is_mapped_class"]
