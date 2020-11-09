c7e3916d1611885eaf0972906e2e165c3270f09e
ThaRising <kochbe.ber@gmail.com>
2020-11-09 12:55:24 +0100

    Allow encoding of declarative instance properties

--- drizm_commons/sqla/encode.py
+++ drizm_commons/sqla/encode.py
@@ -2,6 +2,7 @@ import datetime
 import json
 from typing import Any
 
+from sqlalchemy import Table
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from . import SQLAIntrospector
@@ -12,7 +13,7 @@ class SqlaDeclarativeEncoder(json.JSONEncoder):
     A custom JSON encoder for serializing SQLAlchemy
     declarative base instances.
 
-    Will convert datetime formats to ISO8601 compliant strings.
+    Supports ISO8601 compliant datetime encoding.
     """
     datetypes = (
         datetime.date,
@@ -21,26 +22,47 @@ class SqlaDeclarativeEncoder(json.JSONEncoder):
     )
 
     def default(self, o: Any) -> Any:
-        # Check if the class is an SQLAlchemy declarative table instance
+        # Check if the object is an SQLAlchemy declarative instance
         if isinstance(o.__class__, DeclarativeMeta):
+            fields = {}
+            # Get all fields of the class
+            for field in SQLAIntrospector(o).column_attrs:
+                data = self._process_column(o, field)
+                fields[field] = data
+            return fields
+
+        # Or if it is an SQLAlchemy table instance
+        elif isinstance(o, Table):
             fields = {}
             # Get all columns of the table
             for field in [c.key for c in SQLAIntrospector(o).columns]:
-                # Obtain the value of the field
-                data = getattr(o, field)
-                if data.__class__ in self.datetypes:
-                    data = self.serialize_datetypes_to_iso(data)
-                try:  # Try JSON encoding the field
-                    json.dumps(data)
-                    fields[field] = data
-                except TypeError as exc:  # if it fails resort to failure hook
-                    self.handle_failure(exc, data)
+                data = self._process_column(o, field)
+                fields[field] = data
             return fields
-        # If it is not an SQLAlchemy table use the default encoder
+
+        # If it is not an SQLAlchemy object, use the default encoder
         return super(SqlaDeclarativeEncoder, self).default(o)
+
+    def _process_column(self, o: Any, column_name: str):
+        # Obtain the value of the column and check for custom encoding
+        data = self.dump(
+            getattr(o, column_name)
+        )
+        try:  # Try JSON encoding the field
+            json.dumps(data)
+        except TypeError as exc:  # if it fails resort to failure hook
+            data = self.handle_failure(exc, data)
+        return data
+
+    def dump(self, v: Any) -> Any:
+        """ Hook for custom object deserialization """
+        if v.__class__ in self.datetypes:
+            return self.serialize_datetypes_to_iso(v)
+        return v
 
     # noinspection PyMethodMayBeStatic
     def serialize_datetypes_to_iso(self, v: datetypes) -> str:
+        """ Converts datetime formats to ISO8601 compliant strings """
         if isinstance(v, datetime.timedelta):
             return (datetime.datetime.min + v).time().isoformat()
         return v.isoformat()
