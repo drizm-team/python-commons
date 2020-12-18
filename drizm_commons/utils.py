d5a6fd18af6d266dbada6a143eff22891e89b30a
ThaRising <kochbe.ber@gmail.com>
2020-12-19 00:15:20 +0100

    Added camelCase to snake_case name converter

--- drizm_commons/utils.py
+++ drizm_commons/utils.py
@@ -80,6 +80,12 @@ def url_is_http(url: str) -> bool:
     return False
 
 
+def camel_to_snake(name: str) -> str:
+    """ Converts camelCase names, to snake_case. """
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
 def memoize(fn):
     """
     Caches the last passed parameter,
