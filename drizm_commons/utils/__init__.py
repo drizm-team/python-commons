f3f8385d50cc7761507a60b8d3778d0e004720fb
ThaRising <kochbe.ber@gmail.com>
2020-12-30 14:08:14 +0100

    Moved various utils into separate file

--- drizm_commons/utils/__init__.py
+++ drizm_commons/utils/__init__.py
@@ -1,26 +1,17 @@
-import re
-from typing import Iterable
-
 from . import pathing
 from . import tf
 from . import type
-from .decorators import memoize
-
-
-def camel_to_snake(name: str) -> str:
-    """ Converts camelCase names, to snake_case. """
-    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
-
-
-def exclude_keys(dictionary: dict, keys: Iterable) -> dict:
-    """ Return a new dict - the specified keys """
-    return {
-        k: v for k, v in dictionary.items() if k not in keys
-    }
+from .decorators import memoize, resolve_super_auto_resolution
+from .various import camel_to_snake, exclude_keys, decorate_class_object_methods
 
 
 __all__ = [
-    "camel_to_snake", "exclude_keys", "memoize",
-    "pathing", "tf", "type"
+    "camel_to_snake",
+    "exclude_keys",
+    "memoize",
+    "pathing",
+    "tf",
+    "type",
+    "decorate_class_object_methods",
+    "resolve_super_auto_resolution",
 ]
