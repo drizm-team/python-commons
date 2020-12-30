0329b8b3643efc6fccb47b3661a8feced781dfc7
ThaRising <kochbe.ber@gmail.com>
2020-12-30 14:11:19 +0100

    Added additional types and fixed implementation bugs

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,15 +1,23 @@
-from drizm_commons.utils import (
-    Path,
+from drizm_commons.utils.tf import (
     _TfvarsParser,  # noqa not in __all__
     Tfvars
 )
+from drizm_commons.utils.pathing import Path
+from drizm_commons.utils.type import (
+    AttrDict,
+    IterableKeyDictionary
+)
+import pytest
 
 
-def test010_path():
+def test_pathing__path():
+    # We basically just try and instantiate it
+    # if it does not crash, which it used to do,
+    # then we can consider it working
     Path()
 
 
-def test020_tfvars():
+def test_tf__tfvars():
     path = Path(__file__).parents[1] / "data" / "testing.tfvars"
     tfvars = Tfvars(path)
     assert tfvars.vars.db_comment == "Whatever"
@@ -19,3 +27,37 @@ def test020_tfvars():
     assert tfvars.vars.okay_whatever == "hm"
     assert tfvars.vars.yes == 3.5
     assert tfvars.vars.okay == 3
+
+
+def test_type__attr_dict():
+    obj = AttrDict({
+        "test": 3,
+        "something-else": "ok"
+    })
+    assert obj.test
+
+    with pytest.raises(AttributeError):
+        obj.something
+
+    with pytest.raises(AttributeError):
+        getattr(obj, "something")
+
+    assert obj.something_else
+
+
+def test_type__iterable_key_dict():
+    obj = IterableKeyDictionary({
+        3: "ok",
+        ("action", "aktion"): "return",
+        (5, 7): "yes"
+    })
+
+    assert obj[3]
+    assert obj["action"]
+
+    with pytest.raises(KeyError):
+        obj["3"]
+
+    assert obj[7]
+
+    assert obj.get((5, 7))
