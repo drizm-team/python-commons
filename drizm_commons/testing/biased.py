b6ded58afe1f2bdea73eac89f7afd82d6a9e163e
ThaRising <kochbe.ber@gmail.com>
2020-12-30 14:08:42 +0100

    Fixed bugs with self_to_id and added better docs

--- drizm_commons/testing/biased.py
+++ drizm_commons/testing/biased.py
@@ -1,32 +1,53 @@
-from typing import Dict, Union
+"""
+Contains utilities that are specifically
+biased towards certain Drizm organization standards.
+
+````python
+from drizm_commons.testing.biased import *
+````
+"""
+from typing import Dict, Union, Optional
 
 
-def self_to_id(body: Dict[str, Union[str, dict]]) -> Union[str, int]:
+def self_to_id(
+    body: Dict[str, Union[str, dict]], force_str: Optional[bool] = False
+) -> Union[str, int]:
     """
-    Looks for the self attribute of a response (like so):
+    Extracts the ID from a Drizm-HATEOAS compliant response body.
+
+    Example body:
+
+    ````json
     {
         "self": {"href": "http://example.net/resources/1/"}
-        ...
     }
-    and then extracts the id out of the provided hyperlink.
+    ````
 
-    :param body: The full JSON response-body, must be compliant
-    with the expected Drizm response-schema
+    For the above body, the returned value would be 1.
+
+    Arguments:
+        body: The JSONified response body.
+        force_str: If `True`, will always output a string,
+            the default behaviour is to try and guess the correct type,
+            which can be either a String (e.g. UUID),
+            or an Integer (Numeric IDs).
+
+    Returns:
+        The extracted identifier.
     """
-    identifier = [
-        i for i in body["self"].get("href").split("/") if i
-    ][-1]
+    identifier = [i for i in body["self"].get("href").split("/") if i][-1]
+    identifier = [i for i in identifier.split("?") if i][0]
 
-    try:
-        # Try and convert this into a numeric id
-        identifier = int(identifier)
+    if not force_str:
+        try:
+            # Try and convert this into a numeric id
+            identifier = int(identifier)
 
-    except TypeError or ValueError:
-        # if that fails, we know it must be something like a UUID
-        pass
+        except TypeError and ValueError:
+            # if that fails, we know it must be something like a UUID
+            pass
 
-    finally:
-        return identifier
+    return identifier
 
 
 __all__ = ["self_to_id"]
